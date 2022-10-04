783e396550033297a86c6417257670b1e264b42a
Maxwell-Wong <78146185@qq.com>
2022-10-04 16:02:51 +0800

    LoupeRunner

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-subprocess_call("Step 12: Sorting blast hits", "python SortBLASTHitsInMemory.py -c " + config.LOUPE_CONFIG_TEMPORARYFILES["ProfilesFolder"] +
-#                 " -o " + config.LOUPE_CONFIG_TEMPORARYFILES["SortedBLASTHitsFolder"] +
-#                 " -p " + config.LOUPE_CONFIG_INPUT["PTYFile"] +
-#                 " -i " + config.LOUPE_CONFIG_TEMPORARYFILES["VicinityIDsFileName"] +
-#                 " -s " + config.LOUPE_CONFIG_TEMPORARYFILES["SeedsExtractedFileName"] +
-#                 " -v " + config.LOUPE_CONFIG_TEMPORARYFILES["VicinityFileName"] +
-#                 " -z " + str(config.LOUPE_CONFIG_INPUT["SortingOverlapThreshold"]) +
-#                 " -x " + str(config.LOUPE_CONFIG_INPUT["SortingCoverageThresold"]))
